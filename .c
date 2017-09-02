11da7a7896bb637ea356bf9a29859e8590739a67
madhumithrakaruppusamy <31558960+madhumithrakaruppusamy@users.noreply.github.com>
2017-09-02 11:19:37 +0530

    Update and rename .gitignore to .c

---
+++ .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n<0)
+{
+printf("negative");
+}
+ifelse(n>0)
+{
+printf("possitive")
+}
+else
+{
+printf("zero")
+}
+getch();
+}
