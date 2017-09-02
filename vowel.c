09a31b32d8fea6409a48c248b3b76ae691fde56d
madhumithrakaruppusamy <31558960+madhumithrakaruppusamy@users.noreply.github.com>
2017-09-02 12:00:58 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -3,7 +3,7 @@ void main()
 {
 char ch;
 scanf("%d",&ch);
-if (char == a||e||i||o||u||A||E||I||O||U||)
+if (ch==a|| ch==e|| ch==i|| ch==o|| ch==u|| ch==A|| ch==E|| ch==I|| ch==O|| ch==U||)
 {
   printf("vowel");
 }
