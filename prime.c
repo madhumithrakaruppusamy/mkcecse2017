a605548ee41aa645b2dd07d7ffb0018013c65d51
madhumithrakaruppusamy <31558960+madhumithrakaruppusamy@users.noreply.github.com>
2017-09-15 14:58:18 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int n, i, flag = 0;
+    int n, i, f = 0;
 
-    printf("Enter a positive integer: ");
+    
     scanf("%d",&n);
 
     for(i=2; i<=n/2; ++i)
@@ -11,12 +11,12 @@ int main()
         // condition for nonprime number
         if(n%i==0)
         {
-            flag=1;
+            f=1;
             break;
         }
     }
 
-    if (flag==0)
+    if (f==0)
         printf("%d is a prime number.",n);
     else
         printf("%d is not a prime number.",n);
